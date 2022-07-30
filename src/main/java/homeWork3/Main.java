5cd04f51e3d69409e53bc2ed6434a9475ab34396
Twinker <qwe@test.ru>
2022-07-30 09:35:42 +0200

    commit to master

--- src/main/java/homeWork3/Main.java
+++ src/main/java/homeWork3/Main.java
@@ -3,7 +3,7 @@ package homeWork3;
 public class Main {
     public static void main(String[] args) {
         if (trueOrFalse()) {
-            System.out.println("Получается, что так");
+            System.out.println("True");
         } else {
             System.out.println("Что-то пошло не так");
         }
