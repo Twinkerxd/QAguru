0f4edf37a55630409972e760da78d4348c62e216
Twinker <qwe@test.ru>
2022-07-30 09:30:48 +0200

    qwe

--- src/main/java/homeWork1/Start.java
+++ src/main/java/homeWork1/Start.java
@@ -2,6 +2,7 @@ package homeWork1;
 
 public class Start {
     public static void main(String[] args) {
+        //
         Man man = new Man("Bob", 30);
         Woman woman = new Woman("Barbara", 25);
 
