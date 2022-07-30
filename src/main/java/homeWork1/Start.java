8b54c01f5464669ff25ce219a68584cd2279c2e0
Twinker <qwe@test.ru>
2022-07-30 09:32:53 +0200

    added homework3

--- src/main/java/homeWork1/Start.java
+++ src/main/java/homeWork1/Start.java
@@ -2,7 +2,6 @@ package homeWork1;
 
 public class Start {
     public static void main(String[] args) {
-        //
         Man man = new Man("Bob", 30);
         Woman woman = new Woman("Barbara", 25);
 
---
+++ src/main/java/homeWork3/Main.java
@@ -0,0 +1,15 @@
+package homeWork3;
+
+public class Main {
+    public static void main(String[] args) {
+        if (trueOrFalse()) {
+            System.out.println("Получается, что так");
+        } else {
+            System.out.println("Что-то пошло не так");
+        }
+    }
+
+    static boolean trueOrFalse() {
+        return true;
+    }
+}
