e99aabb7e83c395de6688d665763f0669bad1146
Twinkerxd <msv-oo@yandex.ru>
2022-08-05 08:27:46 +0200

    Fix homework 5

--- src/test/java/homeWork5/Main.java
+++ src/test/java/homeWork5/Main.java
@@ -3,17 +3,20 @@ package homeWork5;
 import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Main {
     @Test
     void test() {
         open("https://github.com/");
+
         $("input[name='q']").setValue("Selenide").pressEnter();
-        $("a[href='/search?q=Selenide&type=wikis']").click();
-        $("a[title='SoftAssertions']").exists();
-        $("a[title='SoftAssertions']").click();
-        $$("ol li").shouldHave(CollectionCondition.itemWithText("Using JUnit5 extend test class:"));
+        $("a[href='/selenide/selenide']").click();
+        $("#wiki-tab").click();
+        $x("//li/button").click();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").exists();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $$("h4").shouldHave(CollectionCondition.
+                itemWithText("3. Using JUnit5 extend test class:"));
     }
 }
