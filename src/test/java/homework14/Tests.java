869a5b4f389159992328f0bf68bd67667b48b362
Twinkerxd <msv-oo@yandex.ru>
2022-09-21 16:01:03 +0200

    added homeWork 14

--- src/test/java/homework14/Tests.java
+++ src/test/java/homework14/Tests.java
@@ -3,10 +3,7 @@ package homework14;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.exist;
@@ -81,6 +78,17 @@ public class Tests {
         steps.openRepoWithName(REPO_NAME);
         steps.goToPullRequests();
         steps.searchForPullRequestWithName(PR_NAME);
+    }
+
+    @Test
+    void failed() {
+        Assertions.assertTrue(1 > 2);
+    }
+
+    @Test
+    @Disabled
+    void skipped() {
+
     }
 
     @AfterEach
